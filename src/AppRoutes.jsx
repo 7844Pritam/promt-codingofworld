e75fbd5ef09950c04216f3dad1bf29c8d8cf4dfc
7844Pritam <pritamsharma7844@gmail.com>
2025-05-29 07:11:20 +0530

    save

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
 import AddPrompt from "./pages/AddPrompt";
@@ -8,23 +9,27 @@ import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import LoginForm from "./pages/Login";
 import PromptsPage from "./pages/PromptsPage";
-import React from "react";
+import RouteSync from "./RouteSync";
+
 function AppRoutes() {
   const routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/add-prompt", element: <AddPrompt /> },
-    // { path: "/explore", element: <Explore /> },
+    // { path: "/explore", element: <Explore /> }, // temporarily disabled
     { path: "/collections", element: <Collections /> },
     { path: "/community", element: <Community /> },
-    { path: "/login", element: <LoginForm /> }, 
+    { path: "/login", element: <LoginForm /> },
     { path: "/signup", element: <Signup /> },
     { path: "/profile", element: <Profile /> },
     { path: "/prompts", element: <PromptsPage /> },
-    
   ]);
 
-  return routes;
+  return (
+    <>
+      <RouteSync />
+      {routes}
+    </>
+  );
 }
-
 
 export default AppRoutes;
---
+++ src/RouteSync.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const RouteSync = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.parent.postMessage(
+      {
+        type: 'CHILD_ROUTE_CHANGE',
+        path: location.pathname,
+      },
+      '*'
+    );
+  }, [location]);
+
+  return null;
+};
+
+export default RouteSync;
